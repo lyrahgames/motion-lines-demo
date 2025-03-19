d10ea9cba3fb49d7d5c8bb468b3698adefb700ec
lyrahgames <lyrahgames@mailbox.org>
2025-03-19 09:55:17 +0100

    Add dash patterns

--- sources/viewer.cpp
+++ sources/viewer.cpp
@@ -758,7 +758,7 @@ void viewer::render() {
   //
   // glDepthFunc(GL_NEVER);
   background_shader.use();
-  glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+  // glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 
   // oit_clear();
 
@@ -927,8 +927,10 @@ void viewer::render() {
   bundle_shader.try_set("projection", camera.projection_matrix());
   bundle_shader.try_set("view", camera.view_matrix());
   bundle_shader.try_set("viewport", camera.viewport_matrix());
-  bundle_shader.try_set("line_width", 30.0f);
+  bundle_shader.try_set("line_width", 4.0f);
   bundle_shader.try_set("now", time);
+  bundle_shader.try_set("delta", 0.5f);
+  bundle_shader.try_set("char_length", bounding_radius);
   bundle_shader.use();
   //
   glDrawArrays(GL_TRIANGLES, 0, 6 * segments(bundle).size());
@@ -1247,7 +1249,7 @@ void viewer::select_maxmin_vids(size_t count) {
 }
 
 void viewer::select_maxmin_vids() {
-  select_maxmin_vids(40);
+  select_maxmin_vids(100);
 
   compute_motion_line_bundle();
 }
@@ -1924,11 +1926,16 @@ void main() {
   // float light = 0.2 + 0.75 * pow(s, 0.2);
 
   // Toon Shading
-  if (light <= 0.50) light = 0.20;
-  else if (light <= 0.60) light = 0.40;
-  else if (light <= 0.80) light = 0.60;
-  else if (light <= 0.90) light = 0.80;
-  else if (light <= 1.00) light = 1.00;
+  // if (light <= 0.50) light = 0.20;
+  // else if (light <= 0.60) light = 0.40;
+  // else if (light <= 0.80) light = 0.60;
+  // else if (light <= 0.90) light = 0.80;
+  // else if (light <= 1.00) light = 1.00;
+
+  if (light <= 0.8)
+    light = 0.5;
+  else
+    light = 1.0;
 
   float weight = 1.0 - instance / float(time_samples);
 
@@ -2321,11 +2328,17 @@ out float time;
 out float v;
 out float arc;
 
+out float varc;
+out float sarc;
+out float speed;
+
 void main() {
   uint sid = gl_VertexID / 6;
   uint eid = gl_VertexID % 6;
   uint element = elements[eid];
   uint vid = segments[sid].vertex + (element % 2);
+  uint vid1 = segments[sid].vertex;
+  uint vid2 = vid1 + 1;
 
   float s = float(element >> 1) - 0.5;
   v = 2.0 * s;
@@ -2335,6 +2348,10 @@ void main() {
   float z = vertices[vid].depth;
   time = vertices[vid].time;
   arc = arcs[segments[sid].stroke] - vertices[vid].arc;
+  varc = vertices[vid].arc;
+  sarc = arcs[segments[sid].stroke];
+
+  speed = (vertices[vid2].arc - vertices[vid1].arc) / (vertices[vid2].time - vertices[vid1].time);
 
   vec4 r = vec4(x + s * line_width * n, z, 1.0);
 
@@ -2345,28 +2362,33 @@ void main() {
   const auto fs = opengl::fragment_shader{R"##(
 #version 460 core
 
-uniform float now;
+uniform float now = 0.0;
+uniform float delta = 1.0;
+uniform float char_length = 1.0;
 
 in float time;
 in float arc;
 in float v;
+in float varc;
+in float sarc;
+in float speed;
 
 layout (location = 0) out vec4 frag_color;
 
-float colormap_red(float x) {
-    return (1.0 + 1.0 / 63.0) * x - 1.0 / 63.0;
-}
+// float colormap_red(float x) {
+//     return (1.0 + 1.0 / 63.0) * x - 1.0 / 63.0;
+// }
 
-float colormap_green(float x) {
-    return -(1.0 + 1.0 / 63.0) * x + (1.0 + 1.0 / 63.0);
-}
+// float colormap_green(float x) {
+//     return -(1.0 + 1.0 / 63.0) * x + (1.0 + 1.0 / 63.0);
+// }
 
-vec4 colormap(float x) {
-    float r = clamp(colormap_red(x), 0.0, 1.0);
-    float g = clamp(colormap_green(x), 0.0, 1.0);
-    float b = 1.0;
-    return vec4(r, g, b, 1.0);
-}
+// vec4 colormap(float x) {
+//     float r = clamp(colormap_red(x), 0.0, 1.0);
+//     float g = clamp(colormap_green(x), 0.0, 1.0);
+//     float b = 1.0;
+//     return vec4(r, g, b, 1.0);
+// }
 
 // vec4 colormap(float x) {
 //     float v = cos(133.0 * x) * 28.0 + 230.0 * x + 27.0;
@@ -2377,15 +2399,98 @@ vec4 colormap(float x) {
 //     return vec4(v, v, v, 1.0);
 // }
 
-void main() {
-  if (time > now) discard;
-  // frag_color = vec4(vec3(0.0), 1.0);
+float colormap_f(float x) {
+  if (x < 0.8110263645648956) {
+    return (((4.41347880412638E+03 * x - 1.18250308887283E+04) * x + 1.13092070303101E+04) * x - 4.94879610401395E+03) * x + 1.10376673162241E+03;
+  } else {
+    return (4.44045986053970E+02 * x - 1.34196160353499E+03) * x + 9.26518306556645E+02;
+  }
+}
 
+float colormap_red(float x) {
+  if (x < 0.09384074807167053) {
+    return 7.56664615384615E+02 * x + 1.05870769230769E+02;
+  } else if (x < 0.3011957705020905) {
+    return (-2.97052932130813E+02 * x + 4.43575866219751E+02) * x + 1.37867123966178E+02;
+  } else if (x < 0.3963058760920129) {
+    return 8.61868131868288E+01 * x + 2.18562881562874E+02;
+  } else if (x < 0.5) {
+    return 2.19915384615048E+01 * x + 2.44003846153861E+02;
+  } else {
+    return colormap_f(x);
+  }
+}
+
+float colormap_green(float x) {
+  if (x < 0.09568486400411116) {
+    return 2.40631111111111E+02 * x + 1.26495726495727E+00;
+  } else if (x < 0.2945883673263987) {
+    return 7.00971783488427E+02 * x - 4.27826773670273E+01;
+  } else if (x < 0.3971604611945229) {
+    return 5.31775726495706E+02 * x + 7.06051282052287E+00;
+  } else if (x < 0.5) {
+    return 3.64925470085438E+02 * x + 7.33268376068493E+01;
+  } else {
+    return colormap_f(x);
+  }
+}
+
+float colormap_blue(float x) {
+  if (x < 0.09892375498249567) {
+    return 1.30670329670329E+02 * x + 3.12116402116402E+01;
+  } else if (x < 0.1985468629735229) {
+    return 3.33268034188035E+02 * x + 1.11699145299146E+01;
+  } else if (x < 0.2928770209555256) {
+    return 5.36891330891336E+02 * x - 2.92588522588527E+01;
+  } else if (x < 0.4061551302245808) {
+    return 6.60915763546766E+02 * x - 6.55827586206742E+01;
+  } else if (x < 0.5) {
+    return 5.64285714285700E+02 * x - 2.63359683794383E+01;
+  } else {
+    return colormap_f(x);
+  }
+}
+
+vec4 colormap(float x) {
+  float r = clamp(colormap_red(x) / 255.0, 0.0, 1.0);
+  float g = clamp(colormap_green(x) / 255.0, 0.0, 1.0);
+  float b = clamp(colormap_blue(x) / 255.0, 0.0, 1.0);
+  return vec4(r, g, b, 1.0);
+}
+
+void main() {
   const float t = now - time;
-  const float l = arc / 500.0;
-  const float weight = 20.0 * l * l * exp(-10.0 * t);
-  if ((v > weight * sin(100.0 * l) + weight) || (v < weight * sin(100.0 * l) - weight)) discard;
-  frag_color = vec4(vec3(1.0), 0.5 + 0.5 * weight);
+  if ((t < 0.0) || (t > delta)) discard;
+
+  const float begin_mask = smoothstep(0.01, 0.02, arc / char_length);
+  const float end_mask = 1.0 - smoothstep(0.95 * delta, delta, t);
+  const float decay_mask = exp(-2.0 * t / delta);
+  // const float dash_mask = smoothstep(0.20, 0.30, abs(mod(50.0 * varc / char_length, 2.0) - 1.0));
+  const float speed_value = speed * delta / char_length;
+  const float dash_bound = exp(-0.15 * speed_value * speed_value);
+  const float speed_mask = 1.0 - dash_bound;
+  const float dash_u = mod(20.0 * varc / char_length, 2.0) - 1.0;
+  const float dash_mask = smoothstep(0.95 * dash_bound, dash_bound, abs(dash_u));
+
+  float weight = begin_mask * end_mask * decay_mask * dash_mask * speed_mask;
+
+  const float width_mask = (1.0 - smoothstep(0.95 * weight, weight, abs(v)));
+  weight *= width_mask;
+
+  // frag_color = vec4(vec3(0.5), weight);
+  frag_color = vec4(vec3(colormap(weight)), weight);
+
+
+  // const float l = arc / 500.0;
+  // const float speed = arc / t;
+  // const float weight = 20.0 * l * l * exp(-10.0 * t);
+  // if ((v > weight * sin(100.0 * l) + weight) || (v < weight * sin(100.0 * l) - weight)) discard;
+  // float weight = smoothstep(5.0, 10.0, arc) * exp(-4.0 * t) * smoothstep(500.0, 501.0, speed);
+  // float valpha = 1.0 - smoothstep(0.4, 0.9, abs(v));
+  // float dash = smoothstep(0.28, 0.32, abs(mod(varc / 4.0, 2.0) - 1.0));
+  // weight = valpha * weight;
+  // frag_color = mix(vec4(1.0), vec4(vec3(0.2), 1.0), weight);
+  // frag_color = vec4(vec3(0.2), valpha * weight * dash);
   // frag_color = mix(colormap(weight) * vec4(vec3(1.0), 1.0), vec4(vec3(1.0), weight), weight);
   // gl_FragDepth = gl_FragCoord.z + (1.0 - gl_FragCoord.z) * t / time_samples;
 }
